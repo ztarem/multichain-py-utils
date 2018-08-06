4bac16ab6a47b44d9d3e50539d4b5fa242cae857
Zvi Tarem <zvi.tarem@gmail.com>
2018-08-06 15:30:50 +0300

    Rewrite based on list of test transactions.

--- mkchain_script.py
+++ mkchain_script.py
@@ -13,8 +13,9 @@ module_name = Path(__file__).stem
 _logger = logging.getLogger(module_name)
 MULTICHAIN_BIN_DIR = Path("usr", "local", "bin")
 MULTICHAIN_HOME = Path.home() / ".multichain"
+CHAIN_NAME = "chain1"
 
-header = r"""
+HEADER = r"""
 #!/usr/bin/env bash
 export PATH={MCFOLDER}:${{PATH}}
 multichain-cli {CHAIN} stop
@@ -31,99 +32,170 @@ echo "rpcport=$rpc_port" >> {MCCONF}
 
 
 def chain_path(chain_name: str) -> Path:
+    """ Get the folder containing chain `chain_name`. """
     return MULTICHAIN_HOME / chain_name
 
 
 def dq(s: str) -> str:
+    """ Wrap string `s` in double quotes. Escape double quotes inside the string. """
     s = s.replace('"', r'\"')
     return f'"{s}"'
 
 
 def sq(s: str) -> str:
+    """ Wrap string `s` in single quotes. Escape single quotes inside the string. """
     s = s.replace("'", r"\'")
     return f"'{s}'"
 
 
 def hex_data(length: int = 50) -> str:
+    """ Generate a random string with `length` hexadecimal characters. """
     return ''.join(random.choices(string.hexdigits, k=length))
 
 
 def text_data(length: int = 50) -> str:
+    """ Generate a random multichain text data string with `length` characters. """
     message = ''.join(random.choices(string.ascii_letters, k=length))
     return json.dumps({"text": message})
 
 
 def json_data(length: int = 50) -> str:
+    """ Generate a random multichain JSON data string with a message of `length` characters. """
     message = ''.join(random.choices(string.ascii_letters, k=length))
-    return json.dumps(
-        {"json": {"list": [1, 2, 3], "message": message}})
+    return json.dumps({"json": {"list": [1, 2, 3], "message": message}})
 
 
-def raw_command(chain_name: str, *cmd) -> str:
-    return f"multichain-cli {chain_name} {' '.join(cmd)}"
+def raw_command(*cmd) -> str:
+    """ Issue a JSON-API command to chain `CHAIN_NAME`.
+    All other positional arguments are concatenated to form the command.
+    """
+    return f"multichain-cli {CHAIN_NAME} {' '.join(cmd)}"
 
 
-def assign_command(chain_name: str, var_name: str, *cmd) -> str:
-    return f"{var_name}=`{raw_command(chain_name, *cmd)}`"
+def assign_command(var_name: str, *cmd) -> str:
+    """ Assign the result of a JSON-API command to chain `CHAIN_NAME` to shell variable `var_name`.
+    All other positional arguments are concatenated to form the command.
+    """
+    return f"{var_name}=`{raw_command(*cmd)}`"
 
 
-def raw_data_commands(chain_name: str, *cmd) -> List[str]:
-    cmd_list = list(cmd)
-    data_index = next(i for i, v in enumerate(cmd_list) if '{DATA}' in v)
-    template = cmd_list[data_index]
-    data_list = []
-    for f in (lambda: f'"{hex_data().lower()}"', lambda: text_data(), lambda: json_data()):
-        data = template.replace('{DATA}', f())
-        if not all(c in string.hexdigits for c in data[1:-1]):
-            data = sq(data)
-        cmd_list[data_index] = data
-        data_list.append(raw_command(chain_name, *cmd_list))
-    return data_list
+def gen_commands(*cmd, var_name: str = None) -> List[str]:
+    """ Issue JSON-API commands to chain `CHAIN_NAME`.
+    All other positional arguments are concatenated to form the command.
+
+    For example::
+
+        gen_commands('publish', 'stream1', 'key2', '012345')
+
+    Output::
+
+        [
+            multichain-cli chain1 publish stream1 key2 "012345"
+        ]
+
+    If `var_name` is provided, the result of the command is assigned to a shell variable by that name.
+
+    For example::
+
+        gen_commands('publish', 'stream1', 'key2', '012345', var_name='txout')
+
+    Output::
+
+        [
+            txout=`multichain-cli chain1 publish stream1 key2 "012345"`
+        ]
+
+    Some command parts may contain the string "{DATA}". In this case, the functions emits separate commands with data
+    of the types: hex, text, and JSON. Every occurrence of "{DATA}" is replaced with randomly generated data of the
+    correct type.
+
+    For example::
+
+        gen_commands('publish', 'stream1', 'key2', '{DATA}')
+
+    Output::
+
+        [
+            multichain-cli chain1 publish stream1 key2 "a2fecf4afc0adfcafa23bccca61d4c2dee776c3d79dcfa07ec",
+            multichain-cli chain1 publish stream1 key2 '{"text": "aQCMKovbgkjZbuQnZaCopFIzCbXdJXxMKNLwMOGnBIIJkoXMPa"}',
+            multichain-cli chain1 publish stream1 key2 '{"json": {"list": [1, 2, 3], "message": "pvCfvbGLrXFMNoICmfMcINxrJOwXIyTdVbVIOxHXcZVSfuIelV"}}'
+        ]
+    """
+    command_list = []
+    cmd_parts = list(cmd)
+    data_indexes = [i for i, v in enumerate(cmd_parts) if '{DATA}' in v]
+    if data_indexes:
+        templates = [cmd_parts[index] for index in data_indexes]
+        # for f in (lambda: f'"{hex_data().lower()}"', lambda: text_data(), lambda: json_data()):
+        for i, s in enumerate((dq(hex_data().lower()), text_data(), json_data())):
+            for data_index, template in zip(data_indexes, templates):
+                data = template.replace('{DATA}', s)
+                if template != "{DATA}" or i > 0:
+                    data = sq(data)
+                cmd_parts[data_index] = data
+            command_list.append(raw_command(*cmd_parts))
+    else:
+        if var_name:
+            command_list.append(assign_command(var_name, *cmd_parts))
+        else:
+            command_list.append(raw_command(*cmd_parts))
+    return command_list
 
 
-def build_script(chain_name: str) -> List[str]:
-    _logger.debug(f"build_script(chain_name={chain_name!r})")
+def build_script(pause: bool) -> List[str]:
+    _logger.debug(f"build_script(pause={pause})")
     key_names = [f"key{i}" for i in range(10, 20)]
     address_sed = "sed -n -E " + sq(r's/.*"address"\s*:\s*"(\w+)".*/\1/p')
+    multi_items = [
+        {"for": "stream1", "keys": ["key3"], "data": {"text": ''.join(random.choices(string.ascii_letters, k=50))}},
+        {"for": "stream1", "keys": ["key4"], "data": {"text": ''.join(random.choices(string.ascii_letters, k=50))}},
+    ]
 
-    return \
-        [
-            header.format(MCFOLDER=MULTICHAIN_BIN_DIR.resolve(), CHAIN=chain_name,
-                          MCPARAMS=str(chain_path(chain_name) / "params.dat"),
-                          MCCONF=str(chain_path(chain_name) / "multichain.conf")).strip(),
-            assign_command(chain_name, 'cache_ident', 'createbinarycache'),
-            raw_command(chain_name, "appendbinarycache", "$cache_ident",
-                        b'This is a pretty long ASCII chunk in the binary cache'.hex()),
-            assign_command(chain_name, 'txid', 'create', 'stream', 'stream1_restrict',
-                           sq('{"restrict": "offchain,write"}')),
-            raw_command(chain_name, 'getrawtransaction', '$txid', '1'),
-            raw_command(chain_name, 'create', 'stream', 'stream1', 'true'),
-            raw_command(chain_name, 'publish', 'stream1', 'key1', dq('{"cache": "$cache_ident"}')),
-        ] + \
-        raw_data_commands(chain_name, 'publish', 'stream1', 'key2', '{DATA}') + \
-        raw_data_commands(chain_name, 'publish', 'stream1', 'key3', '{DATA}', 'offchain') + \
-        raw_data_commands(chain_name, 'publish', 'stream1', sq(json.dumps(key_names)), '{DATA}') + \
-        [
-            raw_command(chain_name, 'liststreamitems', 'stream1', 'true'),
-            assign_command(chain_name, 'address1', 'listpermissions', 'issue', '|', address_sed),
-            assign_command(chain_name, 'address2', 'createkeypairs', '|', address_sed),
-            assign_command(chain_name, 'address3', 'getnewaddress'),
-            raw_command(chain_name, 'importaddress', '$address2', 'external'),
-            raw_command(chain_name, 'grant', '$address2', 'receive'),
-            raw_command(chain_name, 'grant', '$address3', 'send,receive'),
-            raw_command(chain_name, 'issue', '$address1', 'asset1', '1000', '1', '0',
-                        sq('{"x": ["ex", "X"], "y": "why?"}')),
-            raw_command(chain_name, 'sendfrom', '$address1', '$address3',
-                        sq('{"asset1": 10, "data": {"json": [1, 2, 3]}}')),
-            raw_command(chain_name, 'sendfrom', '$address3', '$address1', sq('{"asset1": 10}')),
-            raw_command(chain_name, 'sendfrom', '$address1', '$address2', sq('{"asset1": 50}')),
-        ] + \
-        raw_data_commands(chain_name, 'sendwithdatafrom', '$address1', '$address2', sq('{"asset1": 10}'), '{DATA}') + \
-        raw_data_commands(chain_name, 'sendfrom', '$address1', '$address2', '{"asset1": 10, "data": {DATA}}') + \
-        [
-            raw_command(chain_name, 'issue', '$address1',
-                        sq('{"name": "asset1X", "open": true, "restrict": "send"}'), '5000', '0.01', '0'),
-        ]
+    commands = (
+            [HEADER.format(MCFOLDER=MULTICHAIN_BIN_DIR.resolve(), CHAIN=CHAIN_NAME,
+                           MCPARAMS=str(chain_path(CHAIN_NAME) / "params.dat"),
+                           MCCONF=str(chain_path(CHAIN_NAME) / "multichain.conf")).strip()]
+            + gen_commands('listpermissions', 'issue', '|', address_sed, var_name='address1')
+            + gen_commands('createkeypairs', '|', address_sed, var_name='address2')
+            + gen_commands('importaddress', '$address2', 'external')
+            + gen_commands('grant', '$address2', 'receive')
+            + gen_commands('create', 'stream', 'stream1', 'true')
+            + gen_commands('sendfrom', '$address1', '$address2', sq('{"": 0}'))
+            + gen_commands('issue', '$address1', sq('{"name": "asset1", "open": true, "restrict": "send"}'),
+                           '1000', '1', '0', sq(json_data()))
+            + gen_commands('issuemore', '$address1', 'asset1', '1000', '0', sq(json_data()))
+            + gen_commands('sendfrom', '$address1', '$address2', sq('{"asset1": 10}'))
+            + gen_commands('sendfrom', '$address1', '$address2', '{"asset1": 10, "data": {DATA}}')
+            + gen_commands('sendwithdatafrom', '$address1', '$address2', sq('{"asset1": 10}'), '{DATA}')
+            + gen_commands('sendwithdatafrom', '$address1', '$address2', sq('{"asset1": 10}'),
+                           '{"for": "stream1", "keys": ["key20"], "data": {DATA}}')
+            + gen_commands('sendwithdatafrom', '$address1', '$address2', sq('{"asset1": 10}'),
+                           '{"for": "stream1", "keys": ["key21"], "options": "offchain", "data": {DATA}}')
+            + gen_commands('listassettransactions', 'asset1', 'true')
+
+            + gen_commands('publish', 'stream1', 'key1', '{DATA}')
+            + gen_commands('publish', 'stream1', 'key2', '{DATA}', 'offchain')
+            + gen_commands('createrawsendfrom', '$address1', dq('{"$address2": 0}'), sq(json.dumps(multi_items)),
+                           'send')
+            + gen_commands('publish', 'stream1', sq(json.dumps(key_names)), '{DATA}')
+            + gen_commands('liststreamitems', 'stream1', 'true')
+
+            # + gen_commands('create', 'stream', 'stream2', 'true', sq(json_data()))
+            # + gen_commands('liststreams', '"*"', 'true')
+
+            + gen_commands('grant', '$address2', 'asset1.issue')
+            + gen_commands('grant', '$address2', 'stream1.write')
+            + gen_commands('listpermissions', '"asset1.*"', '"*"', 'true')
+            + gen_commands('listpermissions', '"stream1.*"', '"*"', 'true')
+
+            + gen_commands('create', 'upgrade', 'upgradeStuff', 'false',
+                           sq(json.dumps({"max-std-element-size": 60000, "max-std-op-drops-count": 7})))
+            + gen_commands('listupgrades')
+            + gen_commands('approvefrom', '$address1', 'upgradeStuff', 'true')
+            + gen_commands('listupgrades')
+    )
+
+    return commands
 
 
 def write_script(script_name: str, commands: List[str]):
@@ -135,28 +207,31 @@ def write_script(script_name: str, commands: List[str]):
 
 
 def get_options():
-    global MULTICHAIN_BIN_DIR
+    global MULTICHAIN_BIN_DIR, CHAIN_NAME
 
     parser = argparse.ArgumentParser(description="Build a script that builds a new chain")
     parser.add_argument("-v", "--verbose", action="store_true", help="write debug messages to log")
     parser.add_argument("--mcfolder", metavar="DIR", default=MULTICHAIN_BIN_DIR,
                         help="folder with multichain binaries (default: %(default)s)")
-    parser.add_argument("-c", "--chain", metavar="NAME", default="chain1",
+    parser.add_argument("-c", "--chain", metavar="NAME", default=CHAIN_NAME,
                         help="chain name (default: %(default)s)")
     parser.add_argument("-s", "--script", metavar="FILE", default="make_chain.sh", help="name of the output script")
+    parser.add_argument("-p", "--pause", action="store_true",
+                        help="Pause mining so all transactions are on the mempool")
 
     options = parser.parse_args()
 
     if options.verbose:
         _logger.setLevel(logging.DEBUG)
-    if options.mcfolder:
-        MULTICHAIN_BIN_DIR = Path(options.mcfolder)
+    MULTICHAIN_BIN_DIR = Path(options.mcfolder)
+    CHAIN_NAME = options.chain
 
     _logger.info(f"{module_name} - {parser.description}")
     if options.mcfolder:
         _logger.info(f"  MC Folder: {options.mcfolder}")
     _logger.info(f"  Chain:     {options.chain}")
     _logger.info(f"  Script:    {options.script}")
+    _logger.info(f"  Pause:     {options.pause}")
 
     return options
 
@@ -164,7 +239,7 @@ def get_options():
 def main():
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s %(levelname)-7s %(message)s")
     options = get_options()
-    commands = build_script(options.chain)
+    commands = build_script(options.pause)
     write_script(options.script, commands)
     return 0
 
